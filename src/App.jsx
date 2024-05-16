eb0cef79de08c0c6544d73b159245ad85863cfa1
chos4n <miguelbalmes200192318@gmail.com>
2024-05-16 23:46:57 +0800

    imported CoreConcepts and data file on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import CoreConcept from "./assets/components/CoreConcepts";
+import { data } from "./data";
+
 function App() {
   return (
     <div>
@@ -10,7 +13,35 @@ function App() {
         </p>
       </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+            <ul>
+              <CoreConcept 
+                image={data[0].image} 
+                title={data[0].title} 
+                description={data[0].description} 
+              />
+
+              <CoreConcept 
+                image={data[1].image} 
+                title={data[1].title} 
+                description={data[1].description} 
+              />
+
+              <CoreConcept 
+                image={data[2].image} 
+                title={data[2].title} 
+                description={data[2].description} 
+              />
+
+              <CoreConcept 
+                image={data[3].image} 
+                title={data[3].title} 
+                description={data[3].description} 
+              />
+              
+            </ul>
+          </section>
+
       </main>
     </div>
   );
