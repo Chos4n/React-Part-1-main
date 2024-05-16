84a6d0707cc01f26e2f6dcb7536cb2a4e4e4df59
chos4n <miguelbalmes200192318@gmail.com>
2024-05-16 23:35:35 +0800

    added CoreConcepts.jsx file on components and moved the images to the img folder

--- src/data.js
+++ src/data.js
@@ -1,27 +1,23 @@
 
-import componentImg from "./assets/img/components.png";
-import JsxImg from "./assets/img/jsx-ui.png";
-import PropImg from "./assets/img/config.png";
-import StateImg from "./assets/img/state-mgmt.png";
 
 export const data =[
   {
-    image: componentImg,
+    image: 'componentImg',
     title: 'Components',
     description: 'The core UI building block- compose the user interface by combining multiple components'
   },
   {
-    image: JsxImg,
+    image: 'JsxImg',
     title: 'JSX',
     description: 'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered'
   },
   {
-    image: PropImg,
+    image: 'PropImg',
     title: 'Props',
     description: 'Make components configurable (and therefore reusable) by passing input data to them'
   },
   {
-    image: StateImg,
+    image: 'StateImg',
     title: 'State',
     description: 'React-managed data which when changed, causes the components to re-render & the UI to update.'
   }
